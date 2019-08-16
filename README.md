8e414a1648314357917b81a4cced436cd0d83f2b
Vanessa Randall <vanessalrandall@gmail.com>
2019-08-15 22:28:32 -0600

    Update README.md

--- README.md
+++ README.md
@@ -1,79 +1,27 @@
-# FlashCards Starter Kit
+# Flashcards
 
-## Fork This Repo
+## About the Game
 
-On the top right corner of this page, click the **Fork** button.
+Are you new to software development and learning about working with arrays? Here's 30 questions to test your knowledge of the return values, required arguments, and definitions of the array prototype methods.
 
-## Setup
+## To Install
 
-Clone down the forked repo (from your GitHub). Since you don't want to name your project "flashcards-starter", you can use an optional argument when you run `git clone` (you replace the `[...]` with the terminal command arguments):
+Clone down the repo <a href="https://github.com/vrandall66">here</a>.
 
-```bash
-git clone [remote-address] [what you want to name the repo]
-```
+To run the game, you will be playing in your terminal, as shown in the example GIF below. Be sure to create a new directory/folder on your computer that you would like to clone the repo into.
 
-Once you have cloned the repo, change into the directory and install the library dependencies. Run:
+In your terminal, change into your new directory, and clone down the repo. In your terminal, run the command node index.js, hit enter, and test your knowledge! May the force be with you.
 
-```bash
-npm install
-```
+![Gif of game running in the terminal](Flashcards.gif)
 
-To verify that it is setup correctly, run `npm test` in your terminal. You should have 5 pending tests in your `Card` test file that show up.
+#### By [Vanessa Randall](https://github.com/vrandall66)
 
-Running `node index.js` from the root of your project should result in the following message being displayed in your terminal: 
+## Built With/Utilizing
 
-```bash
-Node server running on port 3000
-```
+- Javascript ES5 & ES6
+- Mocha and Chai
 
-*Note that you will not need to you run your server until you are instantiating your `Game` class in Iteration 3. Prior to that, you should be using TDD to drive your code. You should check the functionality of your application by running your tests via `npm test`.*
+## General Info
 
-
-## Where to Add Your Code
-
-### JavaScript
-
-**Create all of your feature code files in the `src` directory.**
-
-We will be using the `module.exports` and `require` syntax to share code across files.
-
-## How to View Your Code in Action
-
-Once you are working through Iteration 3, you will want to start your server to test your functionality.
-In the terminal, run:
-
-```bash
-node index.js
-```
-
-When the game is functioning appropriately, you will be prompted for each question, have the opportunity to select an answer, and will be given feedback. You will be able to play through the entire deck of cards:
-
-![flash cards example gif](https://media.giphy.com/media/1zkb1q58eTiTH6D7wc/giphy.gif)
-
----
-
-## Test Files Organization
-
-Similar to feature code, your test code needs to be put in a specific place for it to run successfully.
-
-**Put all of your test files in the `test` directory.** As a convention, all test filenames should end with `-test.js`. For instance: `Round-test.js`.
-
-## Running Your Tests
-
-Run your test suite using the command:
-
-```bash
-npm test
-```
-
-The test results will output to the terminal.
-
----
-
-## Linting Your Code
-
-Run the command in your terminal `npm run lint` to run the linter on your JavaScript code. There will be errors and warnings right from the start in this starter kit - the linter is still running successfully.
-
-Your linter will look at the JavaScript files you have within the `src` directory and the `test` directory. 
-
----
+A project to explore test driven development in Turing's Front-End Engineering course, Module 2.
+The repository can be found <a href="https://github.com/vrandall66/flashcards-starter">here</a> and the project specs can be found <a href="https://frontend.turing.io/projects/flash-cards.html">here</a>.
--- test/Round-test.js
+++ test/Round-test.js
@@ -1,8 +1,8 @@
-const chai = require('chai');
+const chai = require("chai");
 const expect = chai.expect;
-const Round = require('../src/Round');
-const Deck = require('../src/Deck');
-const Card = require('../src/Card');
+const Round = require("../src/Round");
+const Deck = require("../src/Deck");
+const Card = require("../src/Card");
 let card1;
 let card2;
 let card3;
@@ -11,51 +11,70 @@ let round2;
 let round;
 
 beforeEach(() => {
-  card1 = new Card({ id: 1, question: 'What is Robbie\'s favorite animal', answers: ['sea otter', 'pug', 'capybara'], correctAnswer: 'sea otter' });
-  card2 = new Card({ id: 14, question: 'What organ is Khalid missing?', answers: ['spleen', 'appendix', 'gallbladder'], correctAnswer: 'gallbladder' });
-  card3 = new Card({ id: 12, question: 'What is Travis\'s favorite stress reliever?', answers: ['listening to music', 'watching Netflix', 'playing with bubble wrap'], correctAnswer: 'playing with bubble wrap' })
+  card1 = new Card({
+    id: 1,
+    question: "What is Robbie's favorite animal",
+    answers: ["sea otter", "pug", "capybara"],
+    correctAnswer: "sea otter"
+  });
+  card2 = new Card({
+    id: 14,
+    question: "What organ is Khalid missing?",
+    answers: ["spleen", "appendix", "gallbladder"],
+    correctAnswer: "gallbladder"
+  });
+  card3 = new Card({
+    id: 12,
+    question: "What is Travis's favorite stress reliever?",
+    answers: [
+      "listening to music",
+      "watching Netflix",
+      "playing with bubble wrap"
+    ],
+    correctAnswer: "playing with bubble wrap"
+  });
   deck = new Deck([card1, card2, card3]);
   round2 = new Round(deck);
   round = new Round();
 });
 
-describe('Round', function () {
-
-  it('should be a function', function () {
-    expect(Round).to.be.a('function');
+describe("Round", function() {
+  it("should be a function", function() {
+    expect(Round).to.be.a("function");
   });
 
-  it('should be an instance of Round', function () {
+  it("should be an instance of Round", function() {
     expect(round).to.be.an.instanceof(Round);
   });
 
-  it('should store an instance of deck', function () {
+  it("should store an instance of deck", function() {
     expect(round2.deck).to.deep.equal(deck);
   });
 
-  it('should return the current card being played', function () {
+  it("should return the current card being played", function() {
     expect(round2.returnCurrentCard()).to.equal(card1);
   });
 
-  it('should take a turn', function () {
-    expect(round2.takeTurn('pug')).to.equal('incorrect!');
-    expect(round2.takeTurn('gallbladder')).to.equal('correct!');
-    expect(round2.takeTurn('Lex')).to.equal('incorrect!');
+  it("should take a turn", function() {
+    expect(round2.takeTurn("pug")).to.equal("incorrect!");
+    expect(round2.takeTurn("gallbladder")).to.equal("correct!");
+    expect(round2.takeTurn("Lex")).to.equal("incorrect!");
   });
 
-  it('should calculate percentage of correct answers', function () {
-    round2.takeTurn('pug');
-    round2.takeTurn('gallbladder');
-    round2.takeTurn('Fitzgerald');
+  it("should calculate percentage of correct answers", function() {
+    round2.takeTurn("pug");
+    round2.takeTurn("gallbladder");
+    round2.takeTurn("Fitzgerald");
     expect(round2.incorrectAnswers.length).to.equal(2);
     expect(round2.calculatePercentCorrect()).to.equal(67);
   });
 
-  it('should output the percentage of correct answers at the end of the round', function () {
-    round2.takeTurn('pug');
-    round2.takeTurn('gallbladder');
-    round2.takeTurn('Fitzgerald');
-    expect(round2.endRound()).to.equal('** Round over! ** You answered 67% of the questions correctly in 0 seconds!');
+  it("should output the percentage of correct answers at the end of the round", function() {
+    round2.takeTurn("pug");
+    round2.takeTurn("gallbladder");
+    round2.takeTurn("Fitzgerald");
+    expect(round2.endRound()).to.equal(
+      "** Round over! ** You answered 67% of the questions correctly in 0 seconds!"
+    );
   });
-
 });
